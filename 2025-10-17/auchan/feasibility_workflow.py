575b4fa8c49b4c64379bb18c7cd6f2e60c4574f0
Shahanafarvin <shahana50997@gmail.com>
2025-10-22 11:00:35 +0530

    feasibilty work flow updated

--- 2025-10-17/auchan/feasibility_workflow.py
+++ 2025-10-17/auchan/feasibility_workflow.py
@@ -1,8 +1,47 @@
 import requests
-import json
 import unicodedata
 import re
+################################category api urls to crawl products################################
+headers = {
+    "accept": "application/json",
+    "accept-language": "hu",
+    "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI4cG1XclQzWmxWMUFJbXdiMUhWYWE5T1BWSzkzcjhIcyIsImp0aSI6IjgwOTk3M2ZlNzAyNzYzMGE1ZTZkZjQ5MjQ5MmRhNmE0MjhhYzNhNTk1ZmViNTRhYzI2NDI2Y2MxZDdkNzdjNzgzMjc5ZjMxYmJkMWQ2NjFmIiwiaWF0IjoxNzYxMDIzMDY1LjMzOTkzMywibmJmIjoxNzYxMDIzMDY1LjMzOTkzNiwiZXhwIjoxNzYxMTA5NDY1LjMxNTQxLCJzdWIiOiJhbm9uXzNjNTkxZjM4LTMyNDItNDJjOS04ZTNmLTZmNjgwYTJkOTEzOSIsInNjb3BlcyI6W119.k1quA-b6_p1rNqYs7Y4hhRRPqz2rICfjSAb8dEJZoOp46njyn0gpjPyIq_533jTmDjyxMWrzkGxyxjd1E0xKUPGDu461_DRdzQ97SnEEISuaRGXrztymIjN43OZ4K2VLO1sx3rH3T50sB9_Y5kUSYJPgvxLHikzCzWMe9fAKrzX65xmA4JbPiXhKxo58SF-izGgCHHekNyCu2pDXf9Qqa4g_Npc2CzEGcr24RyPY6zwGDA-0G_jTeKO9WfjuZcrXH5ZAtQjbUp0tJ6pUXiS9-07rJvcCS5eEPjoX2uz0enQJk9hKp4gquDaA88GzCBYSzqBAS81jyaLZmI_okdymzQ",
+    "cache-control": "no-cache",
+    "referer": "https://auchan.hu/",
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
+}
+root_url = "https://auchan.hu/api/v2/cache/tree/0?depth=1&cacheSegmentationCode=&hl=hu"
+response = requests.get(root_url, headers=headers)
+print("Requesting root categories:", response.status_code)
 
+if response.status_code == 200:
+    data = response.json()
+    categories = data.get("children", [])
+    for cat in categories:
+        cat_id = cat.get("id")
+        cat_name = cat.get("name")
+        product_count = cat.get("productCount", 0)
+        category_api = f"https://auchan.hu/api/v2/cache/tree/{cat_id}?depth=1&cacheSegmentationCode=&hl=hu"
+        category_products_api = f"https://auchan.hu/api/v2/cache/products?categoryId={cat_id}&page=1&itemsPerPage=12&filters[]=available&filterValues[]=1&cacheSegmentationCode=&hl=hu"
+
+        sub_resp = requests.get(category_api, headers=headers)
+        subcategories_list = []
+
+        if sub_resp.status_code == 200:
+            sub_data = sub_resp.json()
+            sub_children = sub_data.get("children", [])
+            for sub in sub_children:
+                sub_id = sub.get("id")
+                sub_name = sub.get("name")
+                sub_count = sub.get("productCount", 0)
+                sub_api_url = f"https://auchan.hu/api/v2/cache/products?categoryId={sub_id}&page=1&itemsPerPage=12&filters[]=available&filterValues[]=1&cacheSegmentationCode=&hl=hu"
+
+                subcategories_list.append({
+                    "sub_id": sub_id,
+                    "sub_name": sub_name,
+                    "sub_product_count": sub_count,
+                    "subcategory_api": sub_api_url
+                })
 ##############################CRAWLER##############################
 urls = [
     "https://auchan.hu/api/v2/cache/products?categoryId=15398&page=1&itemsPerPage=12&filters%5B%5D=available&filterValues%5B%5D=1&cacheSegmentationCode=&hl=hu",
